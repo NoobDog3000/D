d0209ffeb78c3d77825cf6ea716cf91c4933a200
NoobDog3000 <136165010+NoobDog3000@users.noreply.github.com>
2023-06-22 01:21:12 +0330

    Update @BlackWacker.py

--- @BlackWacker.py
+++ @BlackWacker.py
@@ -2,7 +2,6 @@ from platform import node, system, release; Node, System, Release = node(), syst
 from os import system, name; system('clear' if name == 'posix' else 'cls')
 from re import match, sub
 from threading import Thread, active_count
-import urllib3; urllib3.disable_warnings()
 from time import sleep
 # Channel: @BlackWacker  - :)
 # Coder: @Pooya_Killer - :)
